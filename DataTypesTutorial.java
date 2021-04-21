45dbbf1a86eced8778f8bfd8d3963cd91ea13d01
janvi809 <67974104+janvi809@users.noreply.github.com>
2021-04-21 21:16:33 +0530

    Add files via upload

---
+++ DataTypesTutorial.java
@@ -0,0 +1,11 @@
+package dataTypes;
+
+public class DataTypesTutorial {
+    public static void main(String[] args) {
+
+        {
+            byte x = 2;
+            System.out.println(x);
+        }
+    }
+}
